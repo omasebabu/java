31cea2a47e42e07e730f45a10f6e44560a733dd6
omasebabu <115346280+omasebabu@users.noreply.github.com>
2025-01-17 14:42:08 +0530

    Add files via upload

---
+++ BitwiseOperation.java
@@ -0,0 +1,9 @@
+public class BitwiseOperation{
+public static void main(String[]Args){
+System.out.println("10 & 2 :"+(10&2));
+System.out.println("10 | 2 :"+(10|2));
+System.out.println("10 ^ 2 :"+(10^2));
+System.out.println("10 << 2 :"+(10<<2));
+System.out.println("10 >> 2 :"+(10>>2));
+}
+}
